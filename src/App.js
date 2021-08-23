4b1d8318e7a644f7a3b0c9d9a01c2070bd9064a2
Antonina Molotkova <molotkovaantonina@gmail.com>
2021-08-23 15:08:00 +0300

    add alert

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import Filter from './components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -19,6 +24,12 @@ class App extends Component {
       name,
       number,
     };
+
+    const equalName = this.state.contacts.find(
+      item => item.name === contact.name,
+    );
+
+    if (equalName) return alert(`${contact.name} is already in contacts`);
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, contact],
